3d1f687b06e152174e81e67c2ba6e16482e6acfb
Juan Cruz Mondino <juancm.2000@hotmail.com>
2024-10-29 19:35:41 -0300

    parcialito5EInforme7

---
+++ UTN 2024/Base de datos/Parciales/Parcialitos/Parcialito 5 - SP, Functions y Triggers/Parcialito 5.sql
@@ -0,0 +1,13 @@
+/*
+Muestrame las actividades. 
+La empresa desea empezar a fomentar los cursos para algún tipo de embarcación a la vez. Por ello se necesita crear una rutina "actividades_para_tipos" que reciba el código de tipo de embarcación 
+como parámetro y devuelva la lista de todas las actividades que se pueden realizar para dicho tipo de embarcación, junto con todos los posibles instructores para cada actividad pero que no se esté
+dictando actualmente ningún curso.
+
+Indicar número y nombre de actividad, legajo, nombre y apellido del instructor.
+
+Invocar la rutina con el código de tipo de embarcación 6 para probarla (incluirlo en la entrega)
+*/
+
+
+call actividades_para_tipos(6);
